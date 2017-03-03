b0ffac1c57b221c050a9f83aad838209950ccddf
Kevin Rivera <kevinpr_007@hotmail.com>
2017-03-03 11:14:37 -0400

    Initial Commit

---
+++ js/generalInitializer.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // Initialize Tabs
+  $('#tabs').tab()
+
+  // Initialize datepicker
+  var date_input = $('.Date-Picker')
+  var container = 'body'
+  var options = {
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true
+  }
+  date_input.datepicker(options)
+})
