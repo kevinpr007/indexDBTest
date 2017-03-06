f61192f2bd4fee792d30310f4b0d1a916b20afa3
Kevin Rivera <kevinpr_007@hotmail.com>
2017-03-06 16:04:41 -0400

    Complete with timeout

--- js/myIndexDB.js
+++ js/myIndexDB.js
@@ -1,10 +1,10 @@
 ;(function () {
   this.indexDB = this.indexDB || {}
-  var indexDB = this.indexDB
+  let indexDB = this.indexDB;
 
   // Database
   const DB_NAME = 'HURIS_DB'
-  var versionDB = 1
+  let versionDB = 1
 
   // Object/Table
   const DB_Table = 'Audit'
@@ -22,8 +22,8 @@
     console.error(systemAlert)
   }
 
-  var database
-  var request = window.indexedDB.open(DB_NAME, versionDB)
+  let database
+  let request = window.indexedDB.open(DB_NAME, versionDB)
 
   request.onerror = function (e) {
     console.error('error: ' + e.target.result)
@@ -31,10 +31,75 @@
 
   request.onsuccess = function (e) {
     database = request.result
+
+    indexDB.read = function read (id) {
+      let transaction = database.transaction([DB_Table])
+      let objectStore = transaction.objectStore(DB_Table)
+      let request = objectStore.get(id)
+
+      request.onerror = function (e) {
+        let error = 'Unable to retrieve data from database!'
+        alert(error)
+        console.error = error
+      }
+
+      request.onsuccess = function (e) {
+        // Do something with the request.result!
+        let result = request.result
+        if (result) {
+          alert(result)
+        }else {
+          alert("Record couldn't be found in your database!")
+        }
+      }
+    }
+
+    indexDB.readAll = function readAll () {
+      let objectStore = database.transaction(DB_Table).objectStore(DB_Table)
+      let objectResult = []
+
+      objectStore.openCursor().onsuccess = function (e) {
+        let cursor = e.target.result
+
+        if (cursor) {
+          objectResult.push(cursor.value)
+          cursor.continue()
+        } else {
+          // TODO
+          // alert('No more entries!')
+        }
+      }
+      console.log(objectResult)
+      return objectResult
+    }
+
+    indexDB.add = function add (obj) {
+      let request = database.transaction([DB_Table], 'readwrite')
+        .objectStore(DB_Table)
+        .add(obj)
+
+      request.onsuccess = function (e) {
+        alert('Information successfully added to your database.')
+      }
+
+      request.onerror = function (e) {
+        alert('Unable to add data: ' + e.target.result)
+      }
+    }
+
+    indexDB.remove = function remove (id) {
+      let request = database.transaction([DB_Table], 'readwrite')
+        .objectStore(DB_Table)
+        .delete(id)
+
+      request.onsuccess = function (e) {
+        alert('Information has been removed from your database.')
+      }
+    }
   }
 
   request.onupgradeneeded = function (e) {
-    var database = e.target.result
+    let database = e.target.result
 
     // Delete the old datastore.
     if (database.objectStoreNames.contains(DB_Table)) {
@@ -42,7 +107,7 @@
     }
 
     // Create a new datastore.
-    var store = database.createObjectStore(DB_Table, {
+    let store = database.createObjectStore(DB_Table, {
       keyPath: 'id'
     })
   }
@@ -50,70 +115,5 @@
   // Close the db when the transaction is done
   request.oncomplete = function () {
     database.close()
-  }
-
-  indexDB.read = function read (id) {
-    var transaction = database.transaction([DB_Table])
-    var objectStore = transaction.objectStore(DB_Table)
-    var request = objectStore.get(id)
-
-    request.onerror = function (e) {
-      let error = 'Unable to retrieve data from database!'
-      alert(error)
-      console.error = error
-    }
-
-    request.onsuccess = function (e) {
-      // Do something with the request.result!
-      let result = request.result
-      if (result) {
-        alert(result)
-      }else {
-        alert("Record couldn't be found in your database!")
-      }
-    }
-  }
-
-  indexDB.readAll = function readAll () {
-    var objectStore = database.transaction(DB_Table).objectStore(DB_Table)
-    var objectResult = []
-
-    objectStore.openCursor().onsuccess = function (e) {
-      var cursor = e.target.result
-
-      if (cursor) {
-        objectResult.push(cursor.value)
-        cursor.continue()
-      } else {
-        // TODO
-        // alert('No more entries!')
-      }
-    }
-    console.log(objectResult)
-    return objectResult
-  }
-
-  indexDB.add = function add (obj) {
-    var request = database.transaction([DB_Table], 'readwrite')
-      .objectStore(DB_Table)
-      .add(obj)
-
-    request.onsuccess = function (e) {
-      alert('Information successfully added to your database.')
-    }
-
-    request.onerror = function (e) {
-      alert('Unable to add data: ' + e.target.result)
-    }
-  }
-
-  indexDB.remove = function remove (id) {
-    var request = database.transaction([DB_Table], 'readwrite')
-      .objectStore(DB_Table)
-      .delete(id)
-
-    request.onsuccess = function (e) {
-      alert('Information has been removed from your database.')
-    }
   }
 }())
